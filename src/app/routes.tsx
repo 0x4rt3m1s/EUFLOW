8bee24598f41079abb294c17ea347ef79bb43eb5
Youssef Amine <72136618+youssefouru@users.noreply.github.com>
2026-04-08 22:25:23 +0200

    website

---
+++ src/app/routes.tsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router";
+import SwitzerlandPage from "./pages/SwitzerlandPage";
+import EuropePage from "./pages/EuropePage";
+import HomePage from "./pages/HomePage";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/europe",
+    element: <EuropePage />,
+  },
+  {
+    path: "/switzerland",
+    element: <SwitzerlandPage />,
+  },
+]);
